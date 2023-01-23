8a8757cc1b6a55ebd91795064065d1089ce08a4d
Laverlin <ilaverlin@gmail.com>
2023-01-23 15:50:41 +0100

    fix json prop naming

--- IB.YasDataApi/abstract/route.go
+++ IB.YasDataApi/abstract/route.go
@@ -8,6 +8,6 @@ type Route struct {
 	RouteId    int32		`json:"routeId"`
 	UserId     int64		`json:"userId"`
 	RouteName  string		`json:"routeName"`
-	UploadTime time.Time	`json:"uploadTime"`
+	UploadTime time.Time	`json:"routeDate"`
 	Waypoints  []Waypoint	`json:"waypoints"`
 }
--- IB.YasDataApi/abstract/user.go
+++ IB.YasDataApi/abstract/user.go
@@ -6,7 +6,7 @@ import (
 
 type User struct {
 	UserId       int32		`json:"userId"`
-	PublicId     string		`json:"pubilcId"`
+	PublicId     string		`json:"publicId"`
 	TelegramId   int64		`json:"telegramId"`
 	UserName     string		`json:"userName"`
 	RegisterTime time.Time	`json:"registerTime"`
